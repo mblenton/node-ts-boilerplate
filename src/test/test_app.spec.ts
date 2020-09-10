0c04347cd92a0e0193015dddb1871d1efba29516
Milan Rakos <mrakos@Milans-iMac-Pro.local>
2020-09-10 11:28:21 +0200

    fix: change to async function

--- src/test/test_app.spec.ts
+++ src/test/test_app.spec.ts
@@ -11,20 +11,18 @@ describe("test GET and POST", () => {
     return request(app).get("/").expect(200);
   });
 
-  it("should return body in json", () => {
+  it("should return body in json", async () => {
     const myTestJson = {
       username: "milan.rakos@gmail.com",
       password: "377773",
     };
-    return request(app)
+    const res = await request(app)
       .post("/user")
       .type("json")
-      .send(JSON.stringify(myTestJson))
-      .then((res) => {
-        expect(res.body).have.property("success");
-        expect(res.body.success).to.be.true;
-        expect(res.body.data).to.be.a("object");
-        expect(res.body.data.username).to.equal("milan.rakos@gmail.com");
-      });
+      .send(JSON.stringify(myTestJson));
+    expect(res.body).have.property("success");
+    expect(res.body.success).to.be.true;
+    expect(res.body.data).to.be.a("object");
+    expect(res.body.data.username).to.equal("milan.rakos@gmail.com");
   });
 });
